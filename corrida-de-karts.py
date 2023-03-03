e7ae32a29ea62988c168ecbb7e970e8ec30778a8
Lucas Gabriel Pereira Pestana <lucasgabrielpestana03@gmail.com>
2023-03-02 23:52:55 -0300

    Uptade 3 Corrida de Karts

--- corrida-de-karts.py
+++ corrida-de-karts.py
@@ -1,7 +1,8 @@
 import random
 
 tempos = dict()
-info_corredores = list()
+media_corredores = dict()
+
 
 def calcularMediaDeTempos(voltas_tempo):
     
@@ -11,25 +12,38 @@ def calcularMediaDeTempos(voltas_tempo):
 
 def verificarMelhorVolta():
 
-    for volta in range(1, 11, 1):
-        for corredor in tempos:
-            for corredor2 in tempos:
-              if corredor != corredor2 and tempos[corredor] < tempos[corredor2]: 
-                  menor_tempo = tempos[corredor]
-                  corredor_menor_tempo = nome_corredor
-                  volta_menor_tempo = volta
+    # Não está funcionando
+
+    for volta in range(0, 10, 1):
+        for corredor in tempos.keys():
+            for corredor2 in tempos.keys():
+              if corredor != corredor2 and tempos[corredor][volta] < tempos[corredor2][volta]: 
+                  menor_tempo = tempos[corredor][volta]
+                  corredor_menor_tempo = corredor
+                  volta_menor_tempo = volta + 1 
 
     return [volta_menor_tempo, corredor_menor_tempo, menor_tempo]
 
 def verificarRanking():
-    
-    ranking = dict()
-    pos = 1
 
-    for corredor in tempos:
-        ranking[pos] = [corredor, media_corredores[corredor]]
+    # Não está funcionando
+
+    ranking = dict()            
+    posicoes = list()           # Para evitar a sobrescrição de itens pela referência
+    pos = 1                     
+
+    for corredor in tempos.keys():
+        ranking.fromkeys([pos, corredor], media_corredores[corredor])
+        posicoes.append([pos, corredor, media_corredores[corredor]])
+        pos += 1
     
-    return ranking
+    for pos, corredor, media in ranking.items():
+      for pos2, corredor2, media2 in ranking.items():
+        if corredor != corredor2 and pos > pos2 and media >= media2:
+          posicoes[pos2] = [pos2 + 1, corredor, media] 
+          posicoes[pos] = [pos + 1, corredor2, media2]
+
+    return posicoes
 
 
 for index in range(6):
@@ -41,26 +55,26 @@ for index in range(6):
     for volta in range(10):
         
         # tempo_volta = float(input(f"Digite o tempo da volta {volta + 1}: "))
-        tempo_volta = random.choice(range(1, 121))
+        tempo_volta = random.choice(range(1, 121))              # Para testes
         tempos_volta.append(tempo_volta)
     
     tempos[nome_corredor] = tempos_volta
 
-for corredor in tempos:
+for corredor in tempos.keys():           # Teste com o random
     print(corredor, " = ", tempos[corredor])
 
-for corredor in tempos:
+for corredor in tempos.keys():
     
     media_corredor = calcularMediaDeTempos(tempos[corredor])
 
     info_melhor_volta = verificarMelhorVolta()
 
-    info_corredores.append([corredor,media_corredor])
+    media_corredores[corredor] = media_corredor
 
 
-for pos in range(6):
+for corredor in tempos.keys():              # Teste com o random
     
-    print(info_corredores[pos][0], " = ", info_corredores[pos][1])
+    print(corredor, " = ", media_corredores[corredor])
 
 ranking = verificarRanking()
 
@@ -69,8 +83,6 @@ print("Ranking")
 print(ranking)
 
 print(f"Melhor Volta: {info_melhor_volta[1]}, feito na volta {info_melhor_volta[0]}, com {info_melhor_volta[2]}s")
-
-
 
 
 
